db1a930272b5a2070078a753b544ca16438b8635
Amrit Singh <asingh1092@gmail.com>
2020-01-20 17:51:46 -0500

    initial commit

---
+++ src/main/java/com/singh/springboot/crudhib/CrudhibApplication.java
@@ -0,0 +1,13 @@
+package com.singh.springboot.crudhib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudhibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudhibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singh/springboot/crudhib/CrudhibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singh.springboot.crudhib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudhibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
